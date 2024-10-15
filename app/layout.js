7f0d0ae29573d484644f0261d125ea0a8ba16c72
Ajay Teotia <teotiaajay20@gmail.com>
2024-10-15 11:37:15 +0530

    analytics-speed

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -37,6 +39,8 @@ export default function RootLayout({ children }) {
           >
             {children}
           </ThemeProvider>
+          <SpeedInsights />
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
